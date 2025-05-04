a7ebf3cc24cafbe464e3d87bf2194c2d48e0da23
ProGramiiSt <ps5plus2608@gmail.com>
2025-05-04 14:44:29 +0300

    Первый коммит: структура мини-приложения для Telegram

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { WebApp } from '@twa-dev/sdk';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>MAHA-AI</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App; 
