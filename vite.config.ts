f1682d3884950286baa2a398038d292fc1063e49
ProGramiiSt <ps5plus2608@gmail.com>
2025-05-06 19:49:04 +0300

    fix: обновлена конфигурация сборки для использования директории build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -13,11 +13,12 @@ export default defineConfig(({ mode }) => ({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src")
+      "@": path.resolve(__dirname, "./src")
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
   build: {
+    outDir: 'build',
     sourcemap: true,
     rollupOptions: {
       input: {
@@ -34,4 +35,4 @@ export default defineConfig(({ mode }) => ({
   optimizeDeps: {
     include: ['@/lib/utils/index.ts']
   }
-}));
+});
