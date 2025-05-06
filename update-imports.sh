808bcbc8a4f31d91a7b7e53d845d62f53c64fcdf
ProGramiiSt <ps5plus2608@gmail.com>
2025-05-06 19:44:09 +0300

    fix: обновлены все импорты utils

--- update-imports.sh
+++ update-imports.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find src -type f -name "*.tsx" -exec sed -i '' 's/@\/lib\/utils/@\/utils/g' {} + 
+find src -type f \( -name "*.tsx" -o -name "*.ts" \) -print0 | xargs -0 sed -i '' 's/@\/lib\/utils/@\/utils/g' 
